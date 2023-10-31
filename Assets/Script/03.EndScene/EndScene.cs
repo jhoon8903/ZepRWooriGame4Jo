b3055b06aeb99ed116cd3c3b1e05baa643b3fd83
Daniel Lee <jhoon8903@gmail.com>
2023-10-31 23:14:00 +0900

    End animation => DOTWeen 구현

--- Assets/Script/03.EndScene/EndScene.cs
+++ Assets/Script/03.EndScene/EndScene.cs
@@ -1,19 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class EndScene : MonoBehaviour
 {
-    //button.onclick.AddListner() »ç¿ëÇØº¸·Á°í ½Ãµµ...
+    //button.onclick.AddListner() ï¿½ï¿½ï¿½ï¿½Øºï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ãµï¿½...
     //Button restartBtn;
-    public List<Sprite> members = new List<Sprite>();
+    [Serializable]
+    public class Members
+    {
+        public GameObject memberCharacter;
+        public Sprite idle;
+        public Sprite jump;
+    }
+
+    public List<Members> memberList = new List<Members>();
+
+    public void Start()
+    {
+        float dealy = 0f;
+        foreach (var member in memberList)
+        {
+            SpriteRenderer sr = member.memberCharacter.AddComponent<SpriteRenderer>();
+            Vector2 initPos = member.memberCharacter.transform.position;
+
+            Sequence s = DOTween.Sequence();
+
+            s.AppendInterval(dealy);
+
+            s.AppendCallback(() => sr.sprite = member.jump)
+                .Append(member.memberCharacter.transform.DOLocalMoveY(initPos.y + 0.5f, 0.5f)
+                ).AppendInterval(0.1f);
+
+            s.AppendCallback(() => sr.sprite = member.idle)
+                .Append(member.memberCharacter.transform.DOLocalMoveY(initPos.y, 0.5f));
+
+            s.SetLoops(-1, LoopType.Yoyo);
+
+            dealy += 0.5f;
+        }
+    }
+
     public void Restart()
     {
         //restartBtn = transform.GetChild(0).GetComponent<Button>();
         //restartBtn.onClick.AddListener(() => SceneManager.LoadScene("PlayScene"));
 
-        // °ÔÀÓÇÃ·¹ÀÌ È­¸éÀ¸·Î
+        // ï¿½ï¿½ï¿½ï¿½ï¿½Ã·ï¿½ï¿½ï¿½ È­ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
         SceneManager.LoadScene("Game");
     }
 
@@ -21,7 +57,7 @@ public class EndScene : MonoBehaviour
     {
         //button.onClick.AddListener();
 
-        // ¸ÞÀÎÈ­¸éÀ¸·Î
+        // ï¿½ï¿½ï¿½ï¿½È­ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
         SceneManager.LoadScene("Start");
     }
 }
