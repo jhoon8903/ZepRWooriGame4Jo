e1f9c16b56fbb826f426d2efa4782075538add1a
Sunkunwoo <aksiapice@naver.com>
2023-11-01 11:46:10 +0900

    sunkunwoo
    profile on off

--- Assets/Script/02.GameScene/Profile.cs
+++ Assets/Script/02.GameScene/Profile.cs
@@ -1,8 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Profile : MonoBehaviour
 {
-    // 선건우
+    public GameObject ProfileBox; // 프로파일 UI 지정
+
+    public GameObject ProfileImg1; // 프로파일 UI의 상단 이미지
+
+    public GameObject ProfileImg2; // 프로파일 UI의 하단 이미지
+
+    public Text Name; // 프로파일 UI 이름, MBTI 출력란
+
+    public Text ProfileText; // 프로파일 UI 내용 출력란
+
+    public void Close()
+
+    {
+
+        ProfileBox.SetActive(false); // 닫기 버튼 클릭시 프로필 UI 닫기
+
+    }
 }
